394ecdc699ac2442678ff6735e8df62a018ff94d
Prasiddha subedi <prasiddhasubedi592@gmail.com>
2026-01-23 10:47:09 +0545

    [Poetry] Add script for poem: vtr

---
+++ poetry/vtr/script.js
@@ -0,0 +1,6 @@
+// Poetry page initialization
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Poetry page loaded');
+    
+    // Add any custom animations or interactions here
+});
